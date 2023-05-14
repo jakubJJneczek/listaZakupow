febe8f1be386436535b7e24dfb27cff649e79b02
Jakub <querly000@gmail.com>
2023-05-14 17:13:14 +0200

    Dodaj pliki projektów.

---
+++ listaZakupow/Models/SeedData.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using listaZakupow.Data;
+namespace listaZakupow.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new listaZakupowContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<listaZakupowContext>>()))
+            {
+                if (context == null || context.Produkt == null)
+                {
+                    throw new ArgumentNullException("Null");
+                }
+
+
+                if (context.Produkt.Any())
+                {
+                    return;
+                }
+
+                context.Produkt.AddRange(
+                    new Produkt
+                    {
+                        Opis = "kajzerka",
+                        Rodzaj = "pieczywo",
+                        Data = DateTime.Parse("2023-2-12"),
+                        Zrobione = true,
+
+                    },
+                    new Produkt
+                    {
+                        Opis = "jablko",
+                        Rodzaj = "owoc",
+                        Data = DateTime.Parse("2023-3-12"),
+                        Zrobione = true,
+
+                    }
+                    );
+                context.SaveChanges();
+
+            }
+        }
+    }
+    }
+
